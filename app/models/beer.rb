4453870f898d54d672938c25afbce5335b693e68
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-19 16:46:47 +0200

    add average rating for beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum = 0
+        count = 0
+        self.ratings.each do |rating|
+            count +=1
+            sum +=rating.score
+        end
+        return sum.to_f/count.to_f
+    end
 end
