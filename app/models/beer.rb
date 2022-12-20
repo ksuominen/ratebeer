b09e4ce39d83971cad5c9d3130a84bdb23329b71
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-20 16:26:30 +0200

    Destroy related ratings when beer destroyed

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,6 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
-    has_many :ratings
+    has_many :ratings, dependent: :destroy
 
     def to_s
         (self.name + ", " + self.brewery.name).to_s
