22181967b177dd03eae83b4b670ce738cfc6b831
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-28 09:50:53 +0200

    Add route from signup to new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
   root 'breweries#index'
 
   resources :ratings, only: [:index, :new, :create, :destroy]
+
+  get 'signup', to: 'users#new'
 end
