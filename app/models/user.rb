9b7069ce2d6cf479498963026add052b26e9f4b2
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-28 15:11:23 +0200

    Validate password to have at least 1 number and 1 uppercase letter and length of 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :password, length: { minimum: 4 }
+  validate :check_password
+
+  def check_password
+    upper = password =~ /[A-Z]/
+    num = password =~ /[0-9]/
+    if !upper || !num
+      errors.add(:password, "needs to have at least 1 number and 1 uppercase letter")
+    end
+  end
 
   has_many :ratings
   has_many :beers, through: :ratings
