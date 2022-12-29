ed01e1b41570d5cf2a40bec80552c4650032a9e9
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-29 12:27:14 +0200

    Create tests for beer

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Beer, type: :model do
+  it "is not saved without a name" do 
+    brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create style: "lager", brewery: brewery
+
+    expect(beer).not_to be_valid
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved without a style" do 
+    brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create name: "testiolut", brewery: brewery
+
+    expect(beer).not_to be_valid
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is saved with name, style and brewery" do
+    brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create name: "testiolut", style: "lager", brewery: brewery
+
+    expect(beer).to be_valid
+    expect(Beer.count).to eq(1)
+  end
+end
