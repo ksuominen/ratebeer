e7610e4913fcd646d9245a41e93d7c755e64580d
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-29 19:01:43 +0200

    Show places on navbar, enable special characters in place name search

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@ class PlacesController < ApplicationController
     api_key = "c924d5f59e963d7de3050f8d2fb34220"
     url = "http://beermapping.com/webservice/loccity/#{api_key}/"
     
-    response = HTTParty.get "#{url}#{params[:city]}"
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
     places_from_api = response.parsed_response["bmp_locations"]["location"]
     
     if places_from_api.is_a?(Hash) && places_from_api['id'].nil?
