36afb3985502d94b68a0d35390359f7fe769fcd2
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-29 19:30:01 +0200

    Store bar info in cache

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,10 +1,15 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city) { get_places_in(city) }
+  end
+  
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
-
+  
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
-
+  
     return [] if places.is_a?(Hash) and places['id'].nil?
   
     places = [places] if places.is_a?(Hash)
