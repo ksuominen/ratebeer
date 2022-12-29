d4f62b008b8eba4669242e4b34f54dbab0ed7cba
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-29 12:58:30 +0200

    Create method for favourite style of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,14 @@ class User < ApplicationRecord
 
   def favorite_beer
     return nil if ratings.empty?
+
     ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+
+    beer = ratings.order(score: :desc).limit(1).first.beer
+    beer.style
   end
 end
