31feadc772038cdc0d667243cba9547d2dde7c8e
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2022-12-29 12:44:57 +0200

    Create helper methods for favourite beer tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,31 +53,40 @@ RSpec.describe User, type: :model do
 
   describe "favorite beer" do
     let(:user){ FactoryBot.create(:user) }
-  
-    it "has method for determining one" do
+
+    it "has method for determining the favorite beer" do
       expect(user).to respond_to(:favorite_beer)
     end
-  
-    it "without ratings does not have one" do
+
+    it "without ratings does not have a favorite beer" do
       expect(user.favorite_beer).to eq(nil)
     end
 
     it "is the only rated if only one rating" do
       beer = FactoryBot.create(:beer)
       rating = FactoryBot.create(:rating, score: 20, beer: beer, user: user)
-    
+
       expect(user.favorite_beer).to eq(beer)
     end
 
     it "is the one with highest rating if several rated" do
-      beer1 = FactoryBot.create(:beer)
-      beer2 = FactoryBot.create(:beer)
-      beer3 = FactoryBot.create(:beer)
-      rating1 = FactoryBot.create(:rating, score: 20, beer: beer1, user: user)
-      rating2 = FactoryBot.create(:rating, score: 25, beer: beer2, user: user)
-      rating3 = FactoryBot.create(:rating, score: 9, beer: beer3, user: user)
-    
-      expect(user.favorite_beer).to eq(beer2)
+      create_beers_with_many_ratings({user: user}, 10, 20, 15, 7, 9)
+      best = create_beer_with_rating({ user: user }, 25 )
+
+      expect(user.favorite_beer).to eq(best)
     end
   end
+end 
+
+def create_beer_with_rating(object, score)
+  beer = FactoryBot.create(:beer)
+  FactoryBot.create(:rating, beer: beer, score: score, user: object[:user] )
+  beer
 end
+
+def create_beers_with_many_ratings(object, *scores)
+  scores.each do |score|
+    create_beer_with_rating(object, score)
+  end
+end
+
